247c4f1b6b6d4482ff46c7071337528084c0913a
John Doe <johndoe@example.com>
2021-12-15 14:58:14 +0300

    init commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <title>Replace inside objects</title>
+</head>
+<body>
+    <div class="php-out">
+        PHP: <br/>
+        <?php
+            require __DIR__.'/b-script.php';
+        ?>
+    </div>
+    <div class="js-out">JS: <br /></div>
+
+    <script src="f-script.js"></script>
+</body>
+</html>
